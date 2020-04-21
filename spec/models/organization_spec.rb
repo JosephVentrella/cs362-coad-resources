53f159d741a6fc5c5d19c89c9097e9a140d0ae61
kerojosh96 <jaybirdcook@gmail.com>
2020-04-21 15:50:42 -0700

    added tests in user and organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+it "validates email" do
+	organization = Organization.new
+	expect(organization).to validate_presence_of(:email)
+	expect(organization).to validate_presence_of(:name)
+	expect(organization).to validate_presence_of(:phone)
+	expect(organization).to validate_presence_of(:status)
+	expect(organization).to validate_presence_of(:primary_name)
+	expect(organization).to validate_presence_of(:secondary_name)
+	expect(organization).to validate_presence_of(:secondary_phone)
+end 
+
+it "validates length of description" do
+	organization = Organization.new
+	expect(organization).to validate_length_of(:description).is_at_most(1020)
+  
+end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
 	it {should belong_to(:organization)}
-	it "has email" do
+	it "User has an email" do
 		user = User.new
 		expect(user).to respond_to(:email)
 	end
+
+
+   it "validates email" do
+    user = User.new
+    expect(user).to validate_presence_of(:email)
+   end
+
+   it "validates length of password" do
+   	user = User.new
+   	expect(user).to validate_length_of(:password).is_at_least(7).is_at_most(255)
+   end
+
 end
