388d07cd4e29dc38a9fa2e08144c4f7d426e9347
kerojosh96 <jaybirdcook@gmail.com>
2020-04-23 09:08:23 -0700

    added to user_spec

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-it "exists" do
-r = ResourceCategory.new()
 
-end
 end	
  
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,33 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-	it {should belong_to(:organization)}
-	it "User has an email" do
-		user = User.new
-		expect(user).to respond_to(:email)
+	let(:user){User.new}
+	describe "attributes" do
+	    it "User has an email" do
+			expect(user).to respond_to(:email)
+		end
 	end
 
+	describe "relationship" do
+			it {should belong_to(:organization)} #relationship test
+    end
+    
+   describe "validation" do
 
-   it "validates email" do
-    user = User.new
-    expect(user).to validate_presence_of(:email)
-   end
+    it "validates email" do
+	  	expect(user).to validate_presence_of(:email)
+	  end
 
-   it "validates length of password" do
-   	user = User.new
-   	expect(user).to validate_length_of(:password).is_at_least(7).is_at_most(255)
-   end
+	  it "validates length of password" do
+	  	expect(user).to validate_length_of(:password).is_at_least(7).is_at_most(255)
+	  end
 
+	  it "validates length of email" do
+	  	expect(user).to validate_length_of(:email).is_at_least(1).is_at_most(255)
+	  end
+
+	  it { should validate_uniqueness_of(:email).case_insensitive }
+
+  end
+  
 end
