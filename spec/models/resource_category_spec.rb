35c4f6e1891be77da3ab80d242b7f98034f828a7
JosephVentrella <[ventrejo@oregonstate.edu]>
2020-05-17 17:30:28 -0700

    Refactor resource_category model tests to use factoryReferences:#5

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-	let (:resource_category){ResourceCategory.new()}
+	let (:resource_category){build(:resource_category)}
 
-describe "attributes" do
-	it "exists" do
-		resourcecat = ResourceCategory.new
-	end     
+  describe "attributes" do
+
     it "validates presence of name" do
      expect(resource_category).to validate_presence_of(:name)
     end
@@ -14,51 +12,47 @@ describe "attributes" do
      expect(resource_category).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
     end
     it "validates uniqueness of name" do
-  	  resource_category = ResourceCategory.new
   	  expect(resource_category).to validate_uniqueness_of(:name).case_insensitive
   	end
-end
+  end
+  describe "to_s" do
 
-	it "should print name" do
-      expected_name = "Testy Testinson"
-      resource_category = ResourceCategory.new(name: expected_name)
-      expect(resource_category.to_s).to eq(expected_name)
-	end
+  	it "should print name" do
+      expect(resource_category.to_s).to eq(resource_category.name)
+    end
+  end
 
   describe "inactive?" do
     it "should return true when not active" do
-      r = ResourceCategory.new
-      r.active = (:false)
-      expect(r.inactive?).to eq(true)
+      resource_category.active = (:false)
+      expect(resource_category.inactive?).to eq(true)
     end
   end
 
   describe "deactivate" do
     it " correctly updates to deactivate"do
-     r = ResourceCategory.new
-     r.deactivate
-     expect(r.active).to eq(false)
+     resource_category.deactivate
+     expect(resource_category.active).to eq(false)
     end
   end
 
   describe "activate" do
     it " correctly updates to active"do
-     r = ResourceCategory.new
-     r.activate
-     expect(r.active).to eq(true)
+     resource_category.activate
+     expect(resource_category.active).to eq(true)
      
     end
   end
 
   describe "find_or_create_by" do
-      it "creates a name if one isnt given " do
+      it "creates a the name 'Unspecified' if one isnt given " do
       expect(ResourceCategory.unspecified.name).to eq('Unspecified')
     end
 
-    it "does not creates the name 'unspecified' if one is given " do
+    it "does not create the name 'unspecified' if one is given " do
       test_string = "test"
-      r = ResourceCategory.new(name: test_string)
-      expect(r.name).to_not eq('Unspecified')
+      resource_category = ResourceCategory.new(name: test_string)
+      expect(resource_category.name).to_not eq('Unspecified')
     end
   end
 
