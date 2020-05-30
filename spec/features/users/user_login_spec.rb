83a74206778d3130588481d4bc32d26d1dad3421
JosephVentrella <[ventrejo@oregonstate.edu]>
2020-05-30 16:59:18 -0700

    Add successful login test to user_login_spec References:#09

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+	let(:user) {create(:user)}
 
+	context 'As an organization user' do
+		it "displays the organization dashboard when a user logs in" do
+			visit login_path
+			fill_in "user_email", with: user.email
+			fill_in "user_password", with: user.password
+			click_on 'commit'
+			expect(page).to have_content('Dashboard')
+		end
+	end
 end
