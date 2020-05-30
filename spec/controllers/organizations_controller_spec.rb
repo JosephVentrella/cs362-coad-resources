c0b151677bf078e56a39e5f87a0ab10561784c3f
JosephVentrella <[ventrejo@oregonstate.edu]>
2020-05-30 12:47:49 -0700

    Remove broken test from master

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
+	
 
 end
--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,13 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-before :each do
-	@user = User.build(email:"yoyotest@test.com", password:"thisisatest")
-
-end
-it 'takes us to the dashboard'do
-	log_in_as(@user)
-	expect(page).to have_content('Welcome to your dashboard!')
-end
 
 end
