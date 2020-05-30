73c4ebbd262fc00fd9c0d9c2f9f147a7568a9efb
JosephVentrella <[ventrejo@oregonstate.edu]>
2020-05-30 13:41:00 -0700

    Add user_registration test for a not registered user

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
-
+	context 'As a not registered user' do
+		it "responds with a prompt to confirm email when a valid email is entered" do
+			visit signup_path
+			fill_in "user_email", with: "user@user.com"
+			fill_in "user_password", with: "password"
+			fill_in "user_password_confirmation", with: "password"
+			click_on "commit"
+			expect(page).to have_content("confirmation link")
+		end
+	end
 end
