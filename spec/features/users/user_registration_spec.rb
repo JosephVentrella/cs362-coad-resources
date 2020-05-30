4d4e8355004c5da8239182b0eb27b6b52cbda8ab
JosephVentrella <[ventrejo@oregonstate.edu]>
2020-05-30 14:09:52 -0700

    Add test that does not register user with invalid input

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe 'User registration', type: :feature do
 			click_on "commit"
 			expect(page).to have_content("confirmation link")
 		end
+		it 'does not register a user when input is not valid' do
+			visit signup_path
+			fill_in "user_email", with: "user@user.com"
+			fill_in "user_password", with: "password"
+			fill_in "user_password_confirmation", with: 'wrongPassword'
+			click_on "commit"
+			expect(page).to have_content("Password confirmation doesn't match Password")
+		end
 	end
 end
