c338b69e8bcc49d99b2c39775bc39818ecb741ae
JosephVentrella <[ventrejo@oregonstate.edu]>
2020-05-31 11:02:48 -0700

    Test admin use login in user_login_spec References:#09

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,15 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-	let(:user) {create(:user)}
 
 	context 'As an organization user' do
-		it "displays the organization dashboard when a user logs in" do
+		let(:user) {create(:user, :organization)}
+		it "displays the dashboard when a user logs in" do
 			visit login_path
 			fill_in "user_email", with: user.email
 			fill_in "user_password", with: user.password
 			click_on 'commit'
 			expect(page).to have_content('Dashboard')
 		end
+		it "tells the user when the values input are invalid and the login is invalid" do
+			visit login_path
+			fill_in "user_email", with: user.email
+			fill_in "user_password", with: "wrong password"
+			click_on "commit"
+			expect(page).to have_content("Invalid")
+		end
 	end
+	 
+	context "as an admin" do 
+		let(:admin) { create(:user, :admin) }
+   		before (:each) { admin.confirm }
+  		it "displays the admin dashbord when an admin user logs in" do 
+     		visit login_path
+      		fill_in "user_email", with: admin.email
+      		fill_in "user_password", with: admin.password
+      		click_on "commit"
+     		expect(page).to have_content("Dashboard")
+    	end
+    end
 end
