2f4dd66a3201433f491647dfc114966319e5ba02
JosephVentrella <[ventrejo@oregonstate.edu]>
2020-06-01 00:23:14 -0700

    Add sad path for create_ticket References:#11

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Creating a Ticket', type: :feature do
 		@region = Region.create(name: "Test Region")
 		@resource_category = ResourceCategory.create(name: 'Test Category')
 	end
+
 	it 'displays a success message' do
 		visit new_ticket_path
 		fill_in 'Full Name', with: 'Test Name'
@@ -16,4 +17,24 @@ RSpec.describe 'Creating a Ticket', type: :feature do
 		expect(page).to have_content('Submitted')
 	end
 
+	it 'displays an error message when phone number is not valid' do
+		visit new_ticket_path
+		fill_in 'Full Name', with: 'Test Name'
+		fill_in 'Phone Number', with: 'NOT VALID'
+		select @region.name, from: 'Region'
+		select @resource_category.name, from: 'Resource Category'
+		fill_in 'Description', with: 'TEST FAKE'
+		click_on 'Send this help request'
+		expect(page).to have_content('error')
+	end
+		it 'displays an error message when Full Name is not valid' do
+		visit new_ticket_path
+		fill_in 'Full Name', with: ''
+		fill_in 'Phone Number', with: '4802580854'
+		select @region.name, from: 'Region'
+		select @resource_category.name, from: 'Resource Category'
+		fill_in 'Description', with: 'TEST FAKE'
+		click_on 'Send this help request'
+		expect(page).to have_content('error')
+	end
 end
